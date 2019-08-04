3ca8c2f2b3e5433c492433521dca252b839d0852
Bobby Brady <bobbybrady@Bobbys-MacBook-Pro-2.local>
2019-08-04 16:47:39 -0500

    added scrolling

--- scripts/main.js
+++ scripts/main.js
@@ -36,21 +36,53 @@ new ScrollMagic.Scene({
 //     .addTo(controller);
 
 var tween = TweenMax.to("#reveal2", 1, { rotation: 360, ease: Linear.easeNone });
-var tween2 = TweenMax.to("#reveal2", 1, { left: 650});
+var tween2 = TweenMax.to("#reveal2", 1, { left: 650 });
 
 // build scene
 var scene = new ScrollMagic.Scene({ triggerElement: "#trigger2", duration: 400 })
     .setTween(tween)
     .setPin("#reveal2", { pushFollowers: false })
-    .addIndicators() // add indicators (requires plugin)
+    // .addIndicators() // add indicators (requires plugin)
     .addTo(controller);
 
-    new ScrollMagic.Scene({ triggerElement: "#trigger2", offset:400, duration: 200 })
+new ScrollMagic.Scene({ triggerElement: "#trigger2", offset: 400, duration: 200 })
     .setTween(tween2)
-    .addIndicators() // add indicators (requires plugin)
+    // .addIndicators() // add indicators (requires plugin)
     .addTo(controller);
 
-    new ScrollMagic.Scene({ triggerElement: "#trigger2", duration: 400 })
-    .setVelocity("#image")
-    .addIndicators() // add indicators (requires plugin)
+
+
+new ScrollMagic.Scene({ triggerElement: "#trigger2", offset: 750, duration: 700 })
+    // .setPin("#reveal2")
+    .setTween(tween3)
+    .setClassToggle("#smaller", "smaller2")
+    // .addIndicators() // add indicators (requires plugin)
     .addTo(controller);
+
+var tween3 = TweenMax.to("#smaller", 1, { right: 400 });
+
+new ScrollMagic.Scene({ triggerElement: "#trigger2", offset: 800, duration: 300 })
+    // .setPin("#reveal2")
+    .setTween(tween3)
+    // .addIndicators() // add indicators (requires plugin)
+    .addTo(controller);
+
+var wipeAnimation = new TimelineMax()
+    .fromTo("div.mainimg3", 1, { x: "-100%" }, { x: "0%", ease: Linear.easeNone })  // in from left
+    .fromTo("div.mainimg4", 1, { x: "100%" }, { x: "0%", ease: Linear.easeNone })  // in from right
+    .fromTo("div.mainimg5", 1, {y: "-100%"}, {y: "0%", ease: Linear.easeNone}) // in from top
+    .fromTo("div.mainimg6", 1, {y: "100%"}, {y: "0%", ease: Linear.easeNone}) // in from top
+
+
+
+    new ScrollMagic.Scene({
+        triggerElement: "#pinContainer",
+        triggerHook: "onLeave",
+        duration: "300%"
+    })
+    .setPin("#pinContainer")
+    .setTween(wipeAnimation)
+    .addIndicators() // add indicators (requires plugin)
+    .addTo(controller)
+
+
